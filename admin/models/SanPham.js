0c5b404ce3d3039a60f13ae9dea8b02f5fbe8f88
quochuy1701 <huyhuynh17012000@gmail.com>
2022-11-25 15:42:15 +0700

    cập nhật code

--- admin/models/SanPham.js
+++ admin/models/SanPham.js
@@ -1,10 +1,10 @@
-function SanPham(name, price,screen,backCamera,frontCamera,img,desc,type){
+function SanPham(name, img, price, frontCamera, backCamera, screen, desc, type) {
     this.name = name;
+    this.img = img;
     this.price = price;
     this.screen = screen;
     this.backCamera = backCamera;
     this.frontCamera = frontCamera;
-    this.img = img;
     this.desc = desc;
     this.type = type;
 }
